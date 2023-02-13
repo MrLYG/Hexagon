f665fc80b4f5a31585c9e556b845dfc2a98d633b
Aaron_Villalobos <avilla4537@gmail.com>
2023-02-12 17:54:47 -0800

    added instruction triggers to scene

--- Assets/Prototype 1/Scripts/GameStart.cs
+++ Assets/Prototype 1/Scripts/GameStart.cs
@@ -10,7 +10,7 @@ public class GameStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameInstruction.text = "Press D & A to move.";
+        //gameInstruction.text = "Press D & A to move.";
         Time.timeScale = 0;
     }
 
