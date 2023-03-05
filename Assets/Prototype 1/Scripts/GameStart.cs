12b0f1bedf75a025a0831315c6576197813fce2e
Aaron_Villalobos <avilla4537@gmail.com>
2023-03-05 01:03:44 -0800

    Level 1 UI changes

--- Assets/Prototype 1/Scripts/GameStart.cs
+++ Assets/Prototype 1/Scripts/GameStart.cs
@@ -10,14 +10,18 @@ public class GameStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //gameInstruction.text = "Press D & A to move.";
         Time.timeScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.D) && gameInstruction.gameObject.CompareTag("D Key"))
+        {
+            Time.timeScale = 1.0f;
+            Destroy(gameObject);
+        }
+        else if(Input.GetKey(KeyCode.A) && gameInstruction.gameObject.CompareTag("A Key"))
         {
             Time.timeScale = 1.0f;
             Destroy(gameObject);
