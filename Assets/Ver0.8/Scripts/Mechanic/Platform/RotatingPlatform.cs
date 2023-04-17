99b0d0edbe9fbc19f59bac2e6f5eaf682a4ecffc
winstonlin0309 <linwen@usc.edu>
2023-04-16 19:44:26 -0700

    add wlY&G

--- Assets/Ver0.8/Scripts/Mechanic/Platform/RotatingPlatform.cs
+++ Assets/Ver0.8/Scripts/Mechanic/Platform/RotatingPlatform.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class RotatingPlatform : IObject
 {
+    public bool clockwise = false;
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.forward, curSpeed * Time.deltaTime);
+        if(clockwise)
+        {
+            transform.Rotate(-1 * Vector3.forward, curSpeed * Time.deltaTime);
+        } else
+        {
+            transform.Rotate(Vector3.forward, curSpeed * Time.deltaTime);
+        }
     }
 }
